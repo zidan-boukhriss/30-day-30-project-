c60b7957fcf1c377455b089577bdb74083f712c7
zidan-boukhriss <zidanboukhriss97@gmail.com>
2026-09-20 14:51:35 +0100

    Add files via upload

---
+++ Day-2/script.js
@@ -0,0 +1,82 @@
+let result = "";
+
+function ShowNumbersOnScreen() {
+  const screen = document.getElementById("current");
+  const numbers = document.querySelectorAll(".number");
+
+  numbers.forEach((number) => {
+    number.addEventListener("click", function () {
+      result += number.dataset.number;
+      screen.textContent = result;
+    });
+  });
+}
+
+function DeleteNumberFromScreen() {
+  const screen = document.getElementById("current");
+  const delete_button = document.getElementById("delete");
+
+  delete_button.onclick = function () {
+    if (screen.textContent == "") {
+      return;
+    }
+    if (result) {
+      result = result.slice(0, -1);
+      screen.textContent = result;
+    }
+  };
+}
+
+function ClearScreen() {
+  const clear_button = document.getElementById("clear");
+  const screen = document.getElementById("current");
+  clear_button.onclick = () => {
+    result = "";
+    screen.textContent = result;
+  };
+}
+
+function doMath() {
+  const screen = document.getElementById("current");
+  const buttons = document.querySelectorAll(".btn-operator-ma");
+
+  buttons.forEach((e) => {
+    e.addEventListener("click", function () {
+      result = result + e.dataset.operation;
+      screen.textContent = result;
+    });
+  });
+}
+
+function showResult() {
+  const showResultButton = document.getElementById("equals");
+  const screen = document.getElementById("current");
+  showResultButton.onclick = () => {
+    try {
+      if(result == ""){return}
+      result = eval(result);
+      screen.textContent = result;
+    } catch (e) {
+      alert("invalid expression");
+    }
+  };
+}
+
+function tog(){
+  document.getElementById('chnger').addEventListener('click' ,
+    function(){
+      result = -result || result
+      document.getElementById('current').textContent =result
+
+    }
+  )
+}
+
+window.onload = (_) => {
+  ShowNumbersOnScreen();
+  DeleteNumberFromScreen();
+  ClearScreen();
+  doMath();
+  showResult();
+  tog()
+};
